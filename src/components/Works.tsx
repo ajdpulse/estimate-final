f521bddc6b955e56607e4b93857d312728fbef0f
ajdpulse <aj.dpulseai@gmail.com>
2026-02-11 11:51:45 +0530

    Placeholder added

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -790,7 +790,7 @@ const handleAddWork = async () => {
                     value={newWork.village || ''}
                     onChange={(e) => setNewWork({ ...newWork, village: e.target.value })}
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                    placeholder={t('addWork.entervillage')}
+                    placeholder="Enter Village"
                   />
                 </div>
                 <div>
@@ -802,7 +802,7 @@ const handleAddWork = async () => {
                     value={newWork.grampanchayat || ''}
                     onChange={(e) => setNewWork({ ...newWork, grampanchayat: e.target.value })}
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                    placeholder={t('addWork.entergrampanchayat')}
+                    placeholder="Enter Grampanchayat"
                   />
                 </div>
 
